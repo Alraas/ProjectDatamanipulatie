018da2c2eea9d4a867d95f18e3556b52547a9d4b
alraas <r0730435@student.thomasmore.be>
2021-05-03 18:35:48 +0200

    Mainwindow

---
+++ AutoVerhuurKantoor_DAL/DatabaseOperations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoVerhuurKantoor_DAL
+{
+    public static class DatabaseOperations
+    {
+    }
+}
